a8a265fab90bbc7ca31a4b9e6edf3a6fb0128546
Shalini2807 <114823736+Shalini2807@users.noreply.github.com>
2022-10-01 22:19:42 +0530

    Add files via upload
    Day1

---
+++ day1/FirstProgram.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+import java.util.Iterator;
+
+public class FirstProgram {
+public static void main(String[] args) {
+	int a =5;
+	int factorial =1;
+	for (int i = 1; i <=a; i++) {
+		factorial=factorial*i;
+		
+	}
+	System.out.println("The factorial of 5"+ " is " +factorial);
+}
+}
+
+
---
+++ day1/MyFirstClass.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class MyFirstClass {
+	int a =5;
+	byte number1 =127;
+	short number2 =32000;
+	int number3 =983746520;
+	long number4 =98374650L;
+	float number5 =12839.9390F;
+	double number6 =12009201.39019201902;
+	char inital ='A';
+	boolean hadbreakfast =true;
+	
+	
+}
+	
+	
+	
